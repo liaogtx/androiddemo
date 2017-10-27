5351d483434388438abde660d544bb0bec835daf
ljh <ljh@DESKTOP-SIHN8NP>
2017-10-27 11:52:14 +0800

    最后一次

--- src/test/TestMain.java
+++ src/test/TestMain.java
@@ -5,6 +5,7 @@ public class TestMain {
 	public static void main(String[] args) {
 		for(int i =1;i<311;i++){
 			System.out.println("<dimen name=\"DIMEN_"+i+"DP\">"+i*1.128+"dp"+"</dimen>");	
+			System.out.println("<dimen name=\"DIMEN_"+i+"DP\">"+i*1.128+"dp"+"</dimen>");	
 		}
 	}
 
