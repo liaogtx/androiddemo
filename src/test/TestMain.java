705fc80acfd2bbbc6ea83bae2bbd07747687b824
ljh <ljh@DESKTOP-SIHN8NP>
2017-10-27 10:59:37 +0800

    hahahhaah

---
+++ src/test/TestMain.java
@@ -0,0 +1,11 @@
+package test;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		for(int i =1;i<311;i++){
+			System.out.println("<dimen name=\"DIMEN_"+i+"DP\">"+i*1.128+"dp"+"</dimen>");	
+		}
+	}
+
+}
