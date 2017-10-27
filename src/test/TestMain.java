a84966bf56c7106021821b0b0a87018e04d83974
ljh <ljh@DESKTOP-SIHN8NP>
2017-10-27 11:30:17 +0800

    测试测试

--- src/test/TestMain.java
+++ src/test/TestMain.java
@@ -6,6 +6,7 @@ public class TestMain {
 		for(int i =1;i<311;i++){
 			System.out.println("<dimen name=\"DIMEN_"+i+"DP\">"+i*1.128+"dp"+"</dimen>");	
 			System.out.println("<dimen name=\"DIMEN_"+i+"DP\">"+i*1.128+"dp"+"</dimen>");	
+			System.out.println("<dimen name=\"DIMEN_"+i+"DP\">"+i*1.128+"dp"+"</dimen>");	
 		}
 	}
 
